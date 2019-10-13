109eb0165a289c7f7e88f06b38168d2d8212f285
Dawn Cronin <dawncronin7@gmail.com>
2019-10-13 23:27:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,5 +133,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languagues][1] = "Assembly"
+     
+     return programmer_hash
 
 end
